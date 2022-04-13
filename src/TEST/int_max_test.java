f821a8f0abc4368e25eb54fdd1e1c8f0809b8565
Marcin Skowroński <skowronskimh@gmail.com>
2022-04-13 19:01:49 +0200

    int_max_Test.01

---
+++ src/TEST/int_max_test.java
@@ -0,0 +1,17 @@
+package TEST;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class int_max_test {
+
+    @Test
+    public void int_max_Test(){
+        //given
+        int higher_number = 7;
+        int lower_number = 5;
+        //when
+        //int result = int_max(higher_number,lower_number);
+        //then
+        //Assertions.assertEquals(higher_number,result);
+    }
+}
