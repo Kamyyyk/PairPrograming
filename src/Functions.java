3610db200cdc8021cd19769d01f1259be6625ad0
Kamyyyk <kamyk962@gmail.com>
2022-04-13 18:26:48 +0200

    wip

--- src/Functions.java
+++ src/Functions.java
@@ -2,6 +2,7 @@ public class Functions {
     public static void main(String[] args) {
         System.out.println("Testowa klasa");
         System.out.println("AEH");
+        System.out.println("idi na hui");
     }
 
 }
