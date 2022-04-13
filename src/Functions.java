f41ded8cc80a35a0df8e602ffbb9c96ab933b02c
Kamyyyk <kamyk962@gmail.com>
2022-04-13 18:30:50 +0200

    wip

--- src/Functions.java
+++ src/Functions.java
@@ -1,9 +1,5 @@
 public class Functions {
     public static void main(String[] args) {
-        System.out.println("Testowa klasa");
-        System.out.println("AEH");
-        System.out.println("idi na hui");
-        System.out.println("Kamil to jebany kozak");
     }
 
 }
