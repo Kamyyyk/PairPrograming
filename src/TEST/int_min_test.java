4e813358a5ff9ac400db674e9ef7c327eebff1ca
Kamyyyk <kamyk962@gmail.com>
2022-04-13 19:37:11 +0200

    red new function int_min()

---
+++ src/TEST/int_min_test.java
@@ -0,0 +1,18 @@
+package TEST;
+import com.company.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class int_min_test {
+
+    @Test
+    public void int_min_Test(){
+        //given
+        int higher_number = 7;
+        int lower_number = 5;
+        //when
+        //int result = Main.int_min(higher_number,lower_number);
+        //then
+        //Assertions.assertEquals(lower_number,result);
+    }
+}
